619fe83728d904c532c18a216e7b1c70cc1214c4
airydev001 <airydev585@gmail.com>
2025-02-21 16:30:36 +0100

    first commit

---
+++ hello_word.java
@@ -0,0 +1,11 @@
+class Hello{
+    public static void main (String a[]){
+        // int first_num = 100;
+        // int second_num = 500;
+        // int result_output = first_num * second_num;
+        // System.out.println("result : " + result_output);
+        long longNumber = 1234556777;
+        float floatNumber = 1.2345f;
+        System.out.println("result: " + longNumber+floatNumber);
+    }
+}
