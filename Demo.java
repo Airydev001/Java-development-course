f0d110aa9f6e706b2f676e041ecb3e64bd3c0fe7
airydev001 <airydev585@gmail.com>
2025-02-23 14:10:31 +0100

    push conditional statement

--- Demo.java
+++ Demo.java
@@ -1,8 +1,24 @@
 public class Demo{
     public static void main(String[] args){
-        int x = 6;
-        int y = 5;
-        boolean result = x<y;
-        System.out.println(result);
+    //     int x = 20;
+    //   if(x > 10)
+    //   System.out.println("This is the right answer");
+    // int x = 8;
+    // int y = 5;
+    // int z = 9;
+
+    // if(x > y && x > z)
+    // System.out.println(x);
+    // else if  (y> z)
+    // System.out.println(y);
+    // else 
+    // System.out.println(z);
+    int n =4;
+    int result = 0;
+    if(n % 2 ==0)
+    result = 10;
+    else 
+    result = 20;
+System.out.println(result);
     }
 }
