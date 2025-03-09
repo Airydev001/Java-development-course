a8abf3bf3c73d3f87aebfc718eb2cf856e7d9272
airydev001 <airydev585@gmail.com>
2025-03-09 19:27:21 +0100

    Using the if condition

--- Demo.java
+++ Demo.java
@@ -13,13 +13,33 @@ public class Demo{
     // System.out.println(y);
     // else 
     // System.out.println(z);
-    int n =4;
-    int result = 0;
-    // if(n % 2 ==0)
-    // result = 10;
-    // else 
-    // result = 20;
-    result = n%2==0 ? 10 : 20;
-System.out.println(result);
+//     int n =4;
+//     int result = 0;
+//     // if(n % 2 ==0)
+//     // result = 10;
+//     // else 
+//     // result = 20;
+//     result = n%2==0 ? 10 : 20;
+// System.out.println(result);
+int n = -1;
+
+if( n == 1)
+System.out.println("Monday");
+else if(n == 2)
+System.out.println("Tuesday");
+else if(n == 3)
+System.out.println("Wednesday");
+else if(n == 4)
+    
+    System.out.println("Thursday");
+else if(n == 5)
+System.out.println("Friday");
+else if(n == 6)
+System.out.println("Saturday");
+else if(n == 7)
+System.out.println("Sunday");
+else    
+
+System.out.println("Invalid day");
     }
 }
