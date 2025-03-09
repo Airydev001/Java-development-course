37ca533a6fdfc77144de24b33293a4c67e81039c
airydev001 <airydev585@gmail.com>
2025-03-09 19:49:17 +0100

    case swtich statement

--- Demo.java
+++ Demo.java
@@ -21,25 +21,31 @@ public class Demo{
 //     // result = 20;
 //     result = n%2==0 ? 10 : 20;
 // System.out.println(result);
-int n = -1;
-
-if( n == 1)
-System.out.println("Monday");
-else if(n == 2)
-System.out.println("Tuesday");
-else if(n == 3)
-System.out.println("Wednesday");
-else if(n == 4)
-    
+int n = 3;
+switch(n){
+    case 1:
+    System.out.println("Monday");
+    break;
+    case 2:
+    System.out.println("Tuesday");
+    break;
+    case 3:
+    System.out.println("Wednesday");
+    break;
+    case 4:
     System.out.println("Thursday");
-else if(n == 5)
-System.out.println("Friday");
-else if(n == 6)
-System.out.println("Saturday");
-else if(n == 7)
-System.out.println("Sunday");
-else    
-
-System.out.println("Invalid day");
+    break;
+    case 5:
+    System.out.println("Friday");
+    break;
+    case 6:
+    System.out.println("Saturday");
+    break;
+    case 7:
+    System.out.println("Sunday");
+    break;
+    default:
+    System.out.println("Invalid day");
+}
     }
 }
